b65f60827258e8a4e408121f572bb8017df4c8a2
Samantha Emily-Rachel Belnavis <me@samantharachelb.ca>
2018-06-18 11:30:25 -0400

    Project Auto Deployment

--- index.js
+++ index.js
@@ -38,18 +38,6 @@ const slack = new SlackWebhook('https://hooks.slack.com/services/T7JLVSR0U/BB9UP
     }
 });
 
-module.exports = app => {
-  // Your code here
-  app.log('Yay, the app was loaded!')
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
-};
-
-
 // this function processes releases and stores the record
 // inside a database
 
